42b8da6eef5dc6e2f543572ca7ae012eb532f527
Élie Deloumeau-Prigent <elie.deloumeau@fr.clara.net>
2021-08-12 15:00:21 +0200

    fix tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,9 +10,17 @@ def test_vim_version(host):
   command = host.run('vim --version')
   assert command.rc == 0
 
-def test_claranet_user(host):
-  user = host.user('claranet')
+def test_claranet1_user(host):
+  user = host.user('claranet1')
   assert user.exists
   assert user.shell == '/bin/bash'
-  assert user.home == '/home/claranet'
+  assert user.home == f'/home/{user.name}'
+  assert user.group == 'claranet1'
+
+def test_claranet2_user(host):
+  user = host.user('claranet2')
+  assert user.exists
+  assert user.shell == '/bin/bash'
+  assert user.home == f'/home/{user.name}'
   assert user.group == 'adm'
+  assert 'daemon' in user.groups
