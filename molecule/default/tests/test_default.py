300568f89b3978b64429db6e3241cf140bc222f9
Élie Deloumeau-Prigent <elie.deloumeau@fr.clara.net>
2021-08-12 18:13:58 +0200

    fix tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import stat
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -24,3 +25,46 @@ def test_claranet2_user(host):
   assert user.home == f'/home/{user.name}'
   assert user.group == 'adm'
   assert 'daemon' in user.groups
+
+def test_claranet1_profile_file(host):
+  user_name = 'claranet1'
+  file_name = f'/home/{user_name}/.profile'
+  file = host.file(file_name)
+  assert file.exists
+  assert file.is_file
+  assert file.user == 'root'
+  assert file.group == 'root'
+  assert file.mode == 0o644
+
+def test_claranet1_bash_history_file(host):
+  user_name = 'claranet1'
+  file_name = f'/home/{user_name}/.bash_history'
+  file = host.file(file_name)
+  assert file.exists
+  assert file.is_file
+  assert file.user == user_name
+  assert file.group == user_name
+  assert file.mode == 0o644
+  assert 'a' in host.check_output(f'lsattr {file_name}')[:19]
+
+def test_claranet2_profile_file(host):
+  user_name = 'claranet2'
+  file_name = f'/home/{user_name}/.profile'
+  file = host.file(file_name)
+  assert file.exists
+  assert file.is_file
+  assert file.user == 'root'
+  assert file.group == 'root'
+  assert file.mode == 0o644
+
+def test_claranet2_bash_history_file(host):
+  user_name = 'claranet2'
+  file_name = f'/home/{user_name}/.bash_history'
+  file = host.file(file_name)
+  assert file.exists
+  assert file.is_file
+  assert file.user == user_name
+  assert file.group == 'adm'
+  assert file.mode == 0o644
+  assert 'a' in host.check_output(f'lsattr {file_name}')[:19]
+
